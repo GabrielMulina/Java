f1d5089dfb9d79d68cac74f14e4e4732cc777e3a
Gabriel Mulina <mulinagabriel@gmail.com>
2025-05-14 22:29:11 -0300

    Create Multiplicacao.java

---
+++ Calculadora/Multiplicacao.java
@@ -0,0 +1,8 @@
+package Calculadora;
+
+public class Multiplicacao {
+    public  float calcularMultiplicacao(float n1, float n2){
+        float multiplicacao = n1 * n2;
+        return multiplicacao;
+    }
+}
